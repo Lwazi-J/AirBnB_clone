3243c752572f42f0e152051d5832a1df49e9bb87
Lwazi-J <lwazijiyane962@gmail.com>
2023-12-09 00:38:35 -0800

    Executable files

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is the Amenity class file"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class that inherits from BaseModel class"""
+
+    name = ""
---
+++ models/base_model.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+'''This module is the Basemodel class file
+'''
+from datetime import datetime
+import uuid
+from models import storage
+
+
+class BaseModel:
+    '''This is the Basemodel class blueprint
+    '''
+
+    def __init__(self, *args, **kwargs):
+        '''Class constructor'''
+
+        if kwargs:
+            if "__class__" in kwargs.keys():
+                del kwargs["__class__"]
+            kwargs["created_at"] = datetime.fromisoformat(kwargs["created_at"])
+            kwargs["updated_at"] = datetime.fromisoformat(kwargs["updated_at"])
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+    def __str__(self):
+        '''This is the user representation
+        of objects'''
+
+        obj_class_name = self.__class__.__name__
+        obj_dict_rep = self.__dict__
+        return ("[{:s}] ({:s}) {}".format(
+            obj_class_name, self.id, obj_dict_rep)
+            )
+
+    def __repr__(self):
+        """Return the string representation of the instance."""
+
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__
+                )
+
+    def save(self):
+        '''Updates public instance attributes
+        with the current time'''
+
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        '''Returns a dictionary containing all
+        keys/values of __dict__ of the instance'''
+
+        obj_dict = {}
+        obj_dict = self.__dict__
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+
+        return obj_dict
